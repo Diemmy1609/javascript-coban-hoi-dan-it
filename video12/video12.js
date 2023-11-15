8a3a4b2366a5f5334ca5844151d109a3b6a7e031
Hoang My <diemmy.mn1231@gmail.com>
2023-11-15 13:59:15 +0700

    chapter12

---
+++ video12/video12.js
@@ -0,0 +1,19 @@
+console.log('Hello world from HTML')
+let arrTop4 = ['MU', 'Liverpool', 'Chelsea', 'Man City'];
+
+// for (let i = 0; i < arrTop4.length; i++) {
+//     console.log('Top:', i + 1, arrTop4[i])
+// }
+
+// var i = 0
+// do {
+//     console.log('Top:', i + 1, arrTop4[i]);
+//     i++;
+// } while (i < 4)
+let i = 0
+let flag = false
+while (!flag) {
+    console.log('check value i:', i)
+    i++
+    if (i === 10) flag = true;
+}
